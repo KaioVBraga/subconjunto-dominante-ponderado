4d5fb36d8012d60bc4339091d632d405bedc6758
kaiovbraga <kaiovbraga2001@gmail.com>
2023-01-15 22:29:10 -0300

    feat: latex

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const fs = require("fs");
 const path = require("path");
 
+const formatNumber = (n, size) => {
+  return Number(n.replace("s", "")).toFixed(size).replace(".", ",");
+};
+
 const main = async () => {
   const testsData = fs.readFileSync(
     path.join(__dirname, "formated_tests_data.txt"),
@@ -71,10 +75,75 @@ const main = async () => {
     )
     .join("\n\n");
 
-  fs.writeFileSync(
-    path.join(__dirname, "compared_data.txt"),
-    formatedTestsData
-  );
+  const latexTable = literatureDataObj
+    .map((data) => {
+      const greedyData = formatedTestsDataObj.find(
+        (d) =>
+          d["Instance"] == data["Instance"] && d["Type"] == "Algoritmo Guloso"
+      );
+
+      const greedyRandom015Data = formatedTestsDataObj.find(
+        (d) =>
+          d["Instance"] == data["Instance"] &&
+          d["Type"] == "Algoritmo Guloso Randomizado"
+      );
+
+      const greedyRandom030Data = formatedTestsDataObj.find(
+        (d) =>
+          d["Instance"] == data["Instance"] &&
+          d["Type"] == "Algoritmo Guloso Randomizado"
+      );
+
+      const greedyRandom050Data = formatedTestsDataObj.find(
+        (d) =>
+          d["Instance"] == data["Instance"] &&
+          d["Type"] == "Algoritmo Guloso Randomizado"
+      );
+
+      const greedyReactiveData = formatedTestsDataObj.find(
+        (d) =>
+          d["Instance"] == data["Instance"] &&
+          d["Type"] == "Algoritmo Guloso Randomizado Reativo"
+      );
+
+      const name = data["Instance"].replace(/\_/g, "\\_");
+      const bestCost = formatNumber(data["Cost"], 2);
+      //   const time = formatNumber(data["Time"], 3);
+
+      //   return `Problem.dat\_50\_50\_3   & 0,0   & {\\bf 0,00}    & 0,282   & {\\bf 0,00}   & 0,141   & {\\bf 0,00}     & \\textbf{0,114}  \\\\ \\hline`;
+      //   return `${name} & ${bestCost} & ${formatNumber(
+      //     greedyData["Comparison Rate"],
+      //     2
+      //   )} & ${formatNumber(
+      //     greedyRandom015Data["Comparison Rate"],
+      //     2
+      //   )} & ${formatNumber(
+      //     greedyRandom030Data["Comparison Rate"],
+      //     2
+      //   )}  & ${formatNumber(
+      //     greedyRandom050Data["Comparison Rate"],
+      //     2
+      //   )} & ${formatNumber(
+      //     greedyReactiveData["Comparison Rate"],
+      //     2
+      //   )}  \\\\ \\hline`;
+
+      return `${name} & ${formatNumber(greedyData["Time"], 5)} & ${formatNumber(
+        greedyRandom015Data["Time"],
+        5
+      )} & ${formatNumber(greedyRandom030Data["Time"], 5)}  & ${formatNumber(
+        greedyRandom050Data["Time"],
+        5
+      )} & ${formatNumber(greedyReactiveData["Time"], 5)}  \\\\ \\hline`;
+    })
+    .join("\n");
+
+  fs.writeFileSync(path.join(__dirname, "latex_table.txt"), latexTable);
+
+  //   fs.writeFileSync(
+  //     path.join(__dirname, "compared_data.txt"),
+  //     formatedTestsData
+  //   );
 };
 
 main();
